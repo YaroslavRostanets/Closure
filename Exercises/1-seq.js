d49a64fd50aa0d5bfc5bed8721100053c1e0eb45
Yaroslav R <rostanets@gmail.com>
2019-12-28 12:35:05 +0200

    upd

--- Exercises/1-seq.js
+++ Exercises/1-seq.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const seq = f => g => {
-  return typeof g === 'number' ? f(g) : seq(x => f(g(x)))
-};
+const seq = f => g => typeof g === 'number' ? f(g) : seq(x => f(g(x)));
 
 module.exports = { seq };
